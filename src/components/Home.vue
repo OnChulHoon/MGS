a5552f2947e3741362bfd973e9b8f240cc950e02
daniel.coco <daniel.coco@mail-ion.net>
2018-12-11 10:11:57 +0900

    #MGS-Project - Library: MDB Version - Insert: Navigation Bar Example - Insert: Sign Up Form Example - Insert: Home.vue, SignUp.vue File Create

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!--Navbar-->
+    <mdb-navbar class="indigo" dark>
+      <!-- Navbar brand -->
+      <mdb-navbar-brand href="/">
+        Navbar
+      </mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item href="#" active>Home</mdb-nav-item>
+          <mdb-nav-item href="#">Features</mdb-nav-item>
+          <mdb-nav-item href="#">Pricing</mdb-nav-item>
+          <!-- Dropdown -->
+          <mdb-dropdown tag="li" class="nav-item">
+            <mdb-dropdown-toggle tag="a" navLink color="indigo" slot="toggle" waves-fixed>Account</mdb-dropdown-toggle>
+            <mdb-dropdown-menu>
+              <mdb-dropdown-item href="/account/sign-up">Sign Up</mdb-dropdown-item>
+              <mdb-dropdown-item>Log In</mdb-dropdown-item>
+            </mdb-dropdown-menu>
+          </mdb-dropdown>
+        </mdb-navbar-nav>
+        <!-- Search form -->
+        <form class="form-inline">
+          <input class="form-control mr-auto p-2" type="text" placeholder="Search" aria-label="Search">
+        </form>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+  </div>
+</template>
+
+<script>
+import {mdbNavbar, mdbNavbarNav, mdbNavbarBrand, mdbNavbarToggler, mdbNavItem, mdbDropdown, mdbDropdownToggle, mdbDropdownItem, mdbDropdownMenu} from 'mdbvue'
+
+export default {
+  name: 'Home',
+  components: {
+    mdbNavbar,
+    mdbNavbarNav,
+    mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavItem,
+    mdbDropdown,
+    mdbDropdownToggle,
+    mdbDropdownItem,
+    mdbDropdownMenu
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/account/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="form">
+    <!-- Card -->
+    <mdb-card>
+      <mdb-card-body>
+        <form>
+          <h1 class="h4 text-center py-4">Sign up</h1>
+          <div id="formInput" class="grey-text">
+            <mdb-input label="Your name" icon="user" group type="text" validate error="wrong" success="right"/>
+            <mdb-input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+            <mdb-input label="Confirm your email" icon="exclamation-triangle" group type="text" validate error="wrong" success="right"/>
+            <mdb-input label="Your password" icon="lock" group type="password" validate/>
+          </div>
+          <div class="text-center py-4 mt-3">
+            <mdb-btn color="cyan" type="submit">Register</mdb-btn>
+          </div>
+        </form>
+      </mdb-card-body>
+    </mdb-card>
+    <!-- Card -->
+  </div>
+</template>
+
+<script>
+import {mdbInput, mdbBtn, mdbIcon, mdbCard, mdbCardBody, mdbCardTitle} from 'mdbvue'
+
+export default {
+  name: 'SignUp',
+  components: {
+    mdbInput,
+    mdbIcon,
+    mdbBtn,
+    mdbCard,
+    mdbCardBody,
+    mdbCardTitle
+  }
+}
+</script>
+
+<style scoped>
+  h3 {
+    font-weight: normal;
+    padding-top: 20px;
+    padding-bottom: 30px;
+  }
+  p {
+    color: #969696;
+    margin-bottom: 0;
+    font-size: 14px;
+  }
+  #form{
+    margin: 0 auto;
+    max-width: 400px;
+  }
+  #formInput{
+    text-align: left;
+  }
+</style>
